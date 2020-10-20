81b09b649d387de62b8232316c0a000052ba283c
KenerWendel <kener_wendel@hotmail.com>
2020-10-20 13:36:50 -0300

    Adicionado Beckend para Salvar Ingredientes

--- web/view/ingrediente/ingrediente.js
+++ web/view/ingrediente/ingrediente.js
@@ -1,13 +1,11 @@
-function enviarCadastro(){
+function salvarItem(){
 
-    let usuario = document.getElementById("usuario");
-    let endereco = document.getElementById("endereco");
+    let form = document.getElementById("addItem");
     let dados = {};
 
-    if(validar(usuario) && validar(endereco)){
-        dados['usuario'] = formularioParaObjeto(usuario);
-        dados['endereco'] = formularioParaObjeto(endereco);
-        requisicao("../../cadastro", alertarResposta, JSON.stringify(dados));
+    if(validar(form)){
+        dados = formularioParaObjeto(form);
+        requisicao("../../salvarIngrediente", alertarResposta, JSON.stringify(dados));
     }
 
 }
@@ -22,7 +20,7 @@ function validar(formulario){
     let sucesso = true;
     Object.values(formulario).reduce(
         (obj, field) => {
-            if (field.value.toString().trim() === ""  && (!field.name == 'complemento')) {
+            if (field.value.toString().trim() === "") {
                 alert("Você precisa preencher todos os campos para se Cadastrar! O Campo "+field.name+" Está Vazio!")
                 sucesso = false;
                 return;
